12ae4406994141a093b37b88e4bef12e49711434
giramos <uo202549@uniovi.es>
2019-05-17 01:07:11 +0200

    fix OnLoad

--- El_Llagar_Online/JAVASCRIPT/analogico.js
+++ El_Llagar_Online/JAVASCRIPT/analogico.js
@@ -11,12 +11,12 @@ function funcionReloj() {
 
     function cambiarHora() {
         //Calcular grados por hora (30deg cada hora, 30*12=360º)
-         var horaActualGrados = horaActual * 30 + "deg";
-         hora.style.transform = "rotate(" + horaActualGrados + ")";
+        var horaActualGrados = horaActual * 30 + "deg";
+        hora.style.transform = "rotate(" + horaActualGrados + ")";
 
         //Calcular grados cada minuto (6deg cada minuto, 6*6=360º)
-         var minutoActualGrados = minutoActual * 6 + "deg";
-         minutos.style.transform = "rotate(" + minutoActualGrados + ")";
+        var minutoActualGrados = minutoActual * 6 + "deg";
+        minutos.style.transform = "rotate(" + minutoActualGrados + ")";
     }
 
     function cambiarSegundos() {
@@ -29,8 +29,11 @@ function funcionReloj() {
     cambiarSegundos();
 
     //Actualizar reloj
-    setTimeout("funcionReloj()",1000);
+    setTimeout("funcionReloj()", 1000);
 
     //Actualizar reloj cada segundo
     //    setTimeout("cambiarSegundos()",1000);
+
 }
+window.addEventListener("load", funcionReloj); // con esta funcion En lugar de poner el onLoad en la etiqueta, puedes 
+//ponerlo en tu fichero .js como un evento de la ventana. Cuando se cargue la ventana, lanza tu función
