6367bdce2df65830c2dfbf1a2a36a57fcefdd075
giramos <uo202549@uniovi.es>
2019-06-09 03:15:21 +0200

    push

--- El_Llagar_Online/JAVASCRIPT/reloj.js
+++ El_Llagar_Online/JAVASCRIPT/reloj.js
@@ -1,54 +1,31 @@
 const reloj = {
-    hours: 0,
-    minutes: 0,
-    seconds: 0,
+    //array meses
     monthNames: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
         "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
+        //array días
     dayNames: [ "Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"],
-    interval: null,
-    dateDom: null,
+    interval: null, 
     newDate: null,
-    hoursDom: null,
-    minutesDom: null,
-    secondsDom: null,
+    dateDom:null,
 
     init: function () {
         let self = this;
-
-        this.newDate = new Date();
+        this.newDate = new Date();// Crrea hora
         this.dateDom = $('#date');
-        this.hoursDom = $('#hours');
-        this.minutesDom = $('#minutes');
-        this.secondsDom = $('#seconds');
         this.interval = setInterval(function () {
             self.intervalCallback.apply(self);
         }, 1000);
-
     },
 
-    toDoubleDigit: function (num) {
-        if (num < 10) {
-            return "0" + parseInt(num, 10);
-        }
-        return num;
-    },
-
-    updateDom: function () {
+    updateDom: function () {//actualizar  objeto DOM
         this.dateDom.text(this.dayNames[this.newDate.getDay()] + " " + this.newDate.getDate()
             + ' ' + this.monthNames[this.newDate.getMonth()] + ' ' + this.newDate.getFullYear());
-        this.hoursDom.text(this.toDoubleDigit(this.hours));
-        this.minutesDom.text(this.toDoubleDigit(this.minutes));
-        this.secondsDom.text(this.toDoubleDigit(this.seconds));
     },
 
     intervalCallback: function () {
         this.newDate.setDate(this.newDate.getDate());
-        this.hours = new Date().getHours();
-        this.minutes = new Date().getMinutes();
-        this.seconds = new Date().getSeconds();
-        this.updateDom();
+      this.updateDom();
     }
-
 };
 
 $(document).ready(function () {
