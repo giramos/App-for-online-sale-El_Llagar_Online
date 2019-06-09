ea03c552d538e89e75f5718619871c2f147a4495
giramos <uo202549@uniovi.es>
2019-06-09 04:53:34 +0200

    analogico changes

--- El_Llagar_Online/JAVASCRIPT/analogico.js
+++ El_Llagar_Online/JAVASCRIPT/analogico.js
@@ -3,34 +3,28 @@ function funcionReloj() {
     var horaActual = momentoActual.getHours();
     var minutoActual = momentoActual.getMinutes();
     var segundoActual = momentoActual.getSeconds();
-
     
     var hora = document.getElementById('hora');
     var minutos = document.getElementById('minutos');//Identificadores 
     var segundos = document.getElementById('segundos');
 
+    var segundoActualGrados = segundoActual * 6 + "deg";//(6deg cada segundo, 6*6=360º)
+    var horaActualGrados = horaActual * 30 + "deg";//(30deg cada hora, 30*12=360º)
+    var minutoActualGrados = minutoActual * 6 + "deg";//(6deg cada minuto, 6*6=360º)
+
     function cambiarSegundos() {
-        
-        var segundoActualGrados = segundoActual * 6 + "deg";//(6deg cada segundo, 6*6=360º)
         segundos.style.transform = "rotate(" + segundoActualGrados + ")";//Calcular grados cada segundo 
     }
 
     function cambiarHora() {
-        
-        var horaActualGrados = horaActual * 30 + "deg";//(30deg cada hora, 30*12=360º)
         hora.style.transform = "rotate(" + horaActualGrados + ")";//Calcular grados por hora 
-
-        
-        var minutoActualGrados = minutoActual * 6 + "deg";//(6deg cada minuto, 6*6=360º)
         minutos.style.transform = "rotate(" + minutoActualGrados + ")";//Calculo grados cada minuto 
     }
 
     cambiarHora();
     cambiarSegundos();
-
     //Actualizar reloj
     setTimeout("funcionReloj()", 1000);
-
     //Actualizar reloj cada segundo
     //    setTimeout("cambiarSegundos()",1000);
 
