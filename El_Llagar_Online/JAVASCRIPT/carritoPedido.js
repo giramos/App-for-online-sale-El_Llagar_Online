202766278899598147b4a81775d7cb95cc1f3648
giramos <uo202549@uniovi.es>
2019-06-09 19:40:18 +0200

    carrito YEah

--- El_Llagar_Online/JAVASCRIPT/carritoPedido.js
+++ El_Llagar_Online/JAVASCRIPT/carritoPedido.js
@@ -4,28 +4,54 @@
 
 class CarritoPedido {
 
-    constructor() {
-        this.variables = [];
-        this.dinero = 0;
-    }
-
+    
+    //function actualizar(nombre, precio)
     actualizar(nombre, precio) {
         var carro = document.getElementById("carro");
-        carro.removeAttribute("hidden");
-        var celda = carro.insertRow(1);
+        // Intentar eliminar un atributo que no existe en el elemento no disparará una excepción.
+        carro.removeAttribute("hidden");//
+        var celda = carro.insertRow(1);//creo un elemento <tr> vacío y lo agrega a una tabla.
+
+        //  inserto una celda en la fila actual.
         var nombreCelda = celda.insertCell(0);
         var precioCelda = celda.insertCell(1);
         nombreCelda.innerText = nombre;
         precioCelda.innerText = precio;
     }
 
+    //function guardar()
+    guardar() {
+        sessionStorage.setItem("pedido", this.getElementos());
+        location.replace("../HTML/formalizarPedido.html");
+    }
+    //function añadir(nombre, precio)
     añadir(nombre, precio) {
+        // Anyadimos el Nodo a nuestro carrito
         this.variables.push({ nombre: nombre, precio: precio });
         this.actualizar(nombre, precio);
-        this.calcularTotal(precio);
+        this.calcularTotal(precio);// Calculo el total
         this.habilitar();
     }
 
+    //function habilitar()
+    habilitar() {
+        var tramitar = document.getElementById("pedido");
+        tramitar.removeAttribute("hidden");
+    }
+
+    //function borrar()
+    borrar() {
+        sessionStorage.clear();
+    }
+
+    //function getElementos()
+    getElementos() {
+        // Convierto las "variables" (objetos JavaScript() en una cadena
+        return JSON.stringify(this.variables);
+    }
+
+
+    //function calcularTotal(precio)
     calcularTotal(precio) {
         var total = document.getElementById("total");
         total.removeAttribute("hidden");
@@ -33,22 +59,9 @@ class CarritoPedido {
         total.innerText = "Total del pedido (en euros): " + this.dinero + "€";
     }
 
-    habilitar() {
-        var tramitar = document.getElementById("pedido");
-        tramitar.removeAttribute("hidden");
-    }
-
-    getElementos() {
-        return JSON.stringify(this.variables);
-    }
-
-    guardar() {
-        sessionStorage.setItem("pedido", this.getElementos());
-        location.replace("../HTML/formalizarPedido.html");
-    }
-
-    borrar() {
-        sessionStorage.clear();
+    constructor() {
+        this.variables = [];
+        this.dinero = 0;
     }
 
 }
