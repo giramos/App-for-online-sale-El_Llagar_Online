45c4d8a54a374a21f955e518a5a1eb3b50b9e685
giramos <uo202549@uniovi.es>
2019-06-09 07:21:08 +0200

    manejo de errores map

--- El_Llagar_Online/JAVASCRIPT/mapa.js
+++ El_Llagar_Online/JAVASCRIPT/mapa.js
@@ -3,23 +3,14 @@
 class Mapa {
     constructor() {
         this.datos = new Map();
-        this.inicializar();
+        navigator.geolocation.getCurrentPosition(this.obtener, this.errores);
         var tamano = $(window).height() - $("a").outerHeight(true) - $("footer").outerHeight(true);
         $("main").css("height", "" + tamano + "px");
     }
 
-    inicializar() {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(this.obtener, this.errores);
-        } else {
-            alert("Geolocación no es soportada por navegador.");
-        }
-    }
-
     obtener(posicion) {
-        var coordenadas = posicion.coords;
-        mapa.datos.set('Latitud', coordenadas.latitude);
-        mapa.datos.set('Longitud', coordenadas.longitude);
+        mapa.datos.set('Latitud', posicion.coords.latitude);
+        mapa.datos.set('Longitud', posicion.coords.longitude);
         mapa.mostrar();
     }
 
@@ -29,9 +20,10 @@ class Mapa {
             lng: this.datos.get("Longitud")
         };
         var mapaOpciones = new google.maps.Map(document.getElementsByTagName('main')[0], {
-            zoom: 12,
+            zoom: 12, //Tamaño del mapa en pixeles (obligatorio)
             center: localizacion,
             mapTypeId: google.maps.MapTypeId.TERRAIN// se ve relieve
+            //mapTypeId: google.maps.MapTypeId.ROADMAP
         });
 
         var infoWindow = new google.maps.InfoWindow({ map: mapaOpciones });
@@ -53,10 +45,8 @@ class Mapa {
             ['El Llagar Online, Monumentos 40B', 43.371686, -5.859772]
         ];
 
-
-        var i;
-        for (i = 0; i < marcadores.length; i++) {
-            marker = new google.maps.Marker({
+        for (var i = 0; i < marcadores.length; i++) {
+            var marker = new google.maps.Marker({
                 position: new google.maps.LatLng(marcadores[i][1], marcadores[i][2]),
                 title: marcadores[i][0],
                 map: mapaOpciones
@@ -67,30 +57,21 @@ class Mapa {
 
     errores(error) {
         //Array para guardar los marcadores
-        var marcadores = [
-            ['El Llagar, Gascona 29: Uniovi EII', 43.354762, -5.851274],
-            ['El Llagar Online, El Paraguas s/n', 43.360859, -5.842452],
-            ['El Llagar Online, Mon 16-26', 43.361240, -5.843515],
-            ['El Llagar Online, Galicia 3', 43.361582, -5.854015],
-            ['El Llagar Online, Monumentos 40B', 43.371686, -5.859772]
-        ];
-
-        var mapaOpciones = new google.maps.Map(document.getElementsByTagName('main')[0], {
-            zoom: 12,
-            center: new google.maps.LatLng(marcadores[0][1], marcadores[0][2]),
-            mapTypeId: google.maps.MapTypeId.TERRAIN// se ve relieve
-        });
-
-        var i, marker;
-        for (i = 0; i < marcadores.length; i++) {
-            marker = new google.maps.Marker({
-                position: new google.maps.LatLng(marcadores[i][1], marcadores[i][2]),
-                title: marcadores[i][0],
-                map: mapaOpciones
-            });
+        switch (error.code) {
+            case error.PERMISSION_DENIED:
+                this.mensaje = "El usuario no permite la petición de geolocalización"
+                break;
+            case error.POSITION_UNAVAILABLE:
+                this.mensaje = "Información de geolocalización no disponible"
+                break;
+            case error.TIMEOUT:
+                this.mensaje = "La petición de geolocalización ha caducado"
+                break;
+            case error.UNKNOWN_ERROR:
+                this.mensaje = "Se ha producido un error desconocido"
+                break;
         }
 
-        console.log('Error: ' + error.message);
     }
 
 }
