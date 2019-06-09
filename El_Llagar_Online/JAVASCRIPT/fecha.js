a3ec6ef064bfb17695bfb42416cdf12ede5f4ce8
giramos <uo202549@uniovi.es>
2019-06-09 05:32:26 +0200

    fixed

--- El_Llagar_Online/JAVASCRIPT/fecha.js
+++ El_Llagar_Online/JAVASCRIPT/fecha.js
@@ -1,9 +1,9 @@
 const reloj = {
     //array meses
-    monthNames: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
+    meses: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
         "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
         //array días
-    dayNames: [ "Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"],
+    dias: [ "Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"],
     interval: null, 
     newDate: null,
     dateDom:null,
@@ -11,15 +11,15 @@ const reloj = {
     init: function () {
         var self = this;
         this.newDate = new Date();// Crrea fecha
-        this.dateDom = $('#date');
+        this.dateDom = $('#date');//id = date
         this.interval = setInterval(function () {
             self.intervalCallback.apply(self);
         }, 1000);//intervalo de tiempo, que repite la ejecución de la función continuamente
     },
 
     updateDom: function () {//actualizar  objeto DOM
-        this.dateDom.text(this.dayNames[this.newDate.getDay()] + " " + this.newDate.getDate()
-            + ' ' + this.monthNames[this.newDate.getMonth()] + ' ' + this.newDate.getFullYear());
+        this.dateDom.text(this.dias[this.newDate.getDay()] + " " + this.newDate.getDate()
+            + ' ' + this.meses[this.newDate.getMonth()] + ' ' + this.newDate.getFullYear());
     },
 
     intervalCallback: function () {//llamada continuada / actualiza y da la fecha
