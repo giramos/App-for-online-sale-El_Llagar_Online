bf74c850bdafd9df20d1c45a465c20c2650c00e7
giramos <uo202549@uniovi.es>
2019-06-10 22:31:09 +0200

    prototipo pedido.js

--- El_Llagar_Online/JAVASCRIPT/pedido.js
+++ El_Llagar_Online/JAVASCRIPT/pedido.js
@@ -7,27 +7,13 @@ class Pedido {
         this.formulario = document.querySelector("form[name='tramitar']");
         this.boton = document.getElementById("enviar");
         this.botonBorrar = document.getElementById("borrar");
-        this.botonBorrar.addEventListener("click", this.resetear);
-        this.boton.addEventListener("click", this.enviarFormulario);
-        this.formulario.addEventListener("invalid", this.validacion, true);
+        this.botonBorrar.addEventListener("click", this.borrar);
+        this.boton.addEventListener("click", this.enviar);
+        this.formulario.addEventListener("invalid", this.validar, true);
         this.formulario.addEventListener("input", this.comprobar);
     }
-    //function validacion(evento)
-    validacion(evento) {
-        var elemento = evento.target;
-        elemento.style.background = "#ff207a";
-    }
-    //function  comprobar(evento)
-    comprobar(evento) {
-        var elemento = evento.target;
-        if (elemento.validity.valid) {
-            elemento.style.background = "#78ff1f";
-        } else {
-            elemento.style.background = "#ff207a";
-        }
-    }
-//function enviarFormulario()
-    enviarFormulario() {
+      //function enviar()
+      enviar() {
         this.formulario = document.querySelector("form[name='tramitar']");
         var valido = this.formulario.checkValidity();
         if (valido) {
@@ -42,7 +28,7 @@ class Pedido {
             $.ajax({
                 url: "../php/tramitarPedido.php",
                 type: "POST",
-                data: {"datosUser": datosUser, "pedido": pedido},
+                data: { "datosUser": datosUser, "pedido": pedido },
                 dataType: "json",
                 success: function (data) {
                     alert(data);
@@ -54,12 +40,34 @@ class Pedido {
             });
         }
     }
-//function resetear()
-    resetear() {
+    //function resetear()
+    borrar() {
         this.formulario = document.querySelector("form[name='tramitar']");
+        //borra la informacion
         this.formulario.reset();
         this.boton = document.getElementById("enviar");
         this.boton.click();
+    }
+    //function validar(evento)
+    validar(evento) {
+        //nombreF=formulario.nombre.value;
+        //if ( nombreF ==' ') {
+        //  alert('Es obligatorio el nombre');}
+        var elemento = evento.target;
+        //devuelve el elemento que desencadenó el evento
+        elemento.style.background = "#ff207a";
+        //color para que cuando se borre o aparezca mal el formato
+    }
+    //function  comprobar(evento)
+    comprobar(evento) {
+        var elemento = evento.target;//devuelve el elemento que desencadenó el evento 
+        if (elemento.validity.valid) {
+            //color para confirmar que el formato es bueno
+            elemento.style.background = "#78ff1f";
+        } else {
+            //color para que cuando se borre o aparezca mal el formato
+            elemento.style.background = "#ff207a";
+        }
     }
 }
 // creamos el pedido
